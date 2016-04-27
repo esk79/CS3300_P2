a85316143650e5c0f622cc106bf8eea67c335e98
Evan King <esk79@cornell.edu>
2016-04-27 11:30:28 -0400

    combined gabe and mine

---
+++ js/headers.js
@@ -0,0 +1,15 @@
+var headerDefinitions = {
+    "HDI":"The HPI (Happy Planet Index) measures what matters: the extent to which countries deliver long, happy, sustainable lives for the people that live in them. The Index uses global data on life expectancy, experienced well-being and Ecological Footprint to calculate this.",
+    "Well-Being": "Arithmetic mean of individual responses to the Ladder of Life question in the Gallup World Poll.  Latest data for each country as at February 2012.",
+    "Life Expectancy": "2011 data taken from UNDP Human Development Report 2011",
+    "GDP/capita": "Taken from World Bank World Development Indicators 2010 ($PPP Current prices). For Bahrain, Iran, Libya, Djibouti and Qatar, Cuba, Kuwait and Zimbabwe 2010 data were not available, so the most upto date WDI data available were used.",
+    "Foot-print": "2008 data taken from Global Footprint Network for 142 countries.",
+    "Governance Index": "Rank of the simple average of all six World Governance Indicators countries based on latest data available for each on 02 June 2012.",
+    "Gender Equality Rank": "Gender Inequality Index: A composite measure reflecting inequality in achievement between women and men in three dimensions: reproductive health, empowerment and the labour market.",
+    "Inequality in education": "Inequality in distribution of years of schooling based on data from household surveys estimated using the Atkinson inequality index.",
+    "Incarceration Rate": "Number of incarcerated individuals per 100,000",
+    "Suicide Rate": "Age standardized suicide rates (per 100,000)",
+    "Homocide Rate": "Homocides per 100,000.",
+    "Income Inequality": "The ratio of the average income of the richest 20% to the poorest 20%.",
+    "Region": "1-Latin America\n2-Western World\n3-Middle East and North Africa\n4-Sub Saharan Africa\n5-South Asia\n6-East Asia\n7-Transition States",
+};
